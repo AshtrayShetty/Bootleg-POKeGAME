63fcbc86c88d46085b0a2f1cd763683bc574d1bf
AshtrayShetty <51413934+AshtrayShetty@users.noreply.github.com>
2020-06-17 01:11:26 +0530

    Add project files.

---
+++ GameConfig/Player.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameConfig
+{
+    public class Player
+    {
+        public String Name { get; set; }
+        public int Money { get; set; }
+        public int Fight { get; set; }
+        public int Wins { get; set; }
+        public ObservableCollection<object> Pokemon { get; set; }
+
+    }
+}
